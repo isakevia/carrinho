62f95df32597cfd798e37a4e273079fd7aa425a8
isakevia <isabellisilva506@gmail.com>
2023-08-18 12:23:42 -0300

    arrumamos o style

--- App.tsx
+++ App.tsx
@@ -1,8 +1,24 @@
 
 import { View, StatusBar, SafeAreaView } from 'react-native';
 import Carrinho from './src/pages/Carrinho';
+import { useFonts,
+  Montserrat_400Regular,
+  Montserrat_700Bold,
+  Montserrat_400Regular_Italic
+} from '@expo-google-fonts/montserrat';
 
 export default function App() {
+
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+    "MontserratItalic": Montserrat_400Regular_Italic
+  })
+
+  if (!fonteCarregada) {
+    <View/>
+  }
+
   return (
     <SafeAreaView>
       <StatusBar/>
--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Image, Dimensions, Text, View } from 'react-native';
 
 import topo from '../../../assets/topo.png';
+import logo from '../../../assets/logo.png';
 
 const width = Dimensions.get('screen').width;
 
@@ -9,13 +10,20 @@ export default function Carrinho () {
     return <>
     <Image source={topo} style={estilos.topo}/>
     <Text style={estilos.titulo}>Detalhe do Carrinho</Text>
-    <View>
-        <Text>Carrinho de Compras</Text>
-        <Text>Fazenda Etec Cidade do Livro</Text>
-        <Text>Uma cesta com produtos selecionados cuidadosamente
+
+    <View style={estilos.carrinho}>
+        <Text style={estilos.titulocarrinho}>Carrinho de Compras</Text>
+
+    <View style={estilos.fazenda}>
+        <Image style={estilos.imagemFazenda} source={logo}/>
+        <Text style={estilos.nomeFazenda}>Fazenda Etec Cidade do Livro</Text>
+    </View>
+
+        <Text style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente
             da fazenda direto para sua cozinha
         </Text>
-        <Text>R$ 40,00</Text>
+
+        <Text style={estilos.preco}>R$ 40,00</Text>
     </View>
     </>
 }
@@ -34,5 +42,46 @@ const estilos = StyleSheet.create({
       color: "white",
       position: "absolute",
       padding: 16
-  }  
+  },
+  
+  titulocarrinho: {
+      fontSize: 26,
+      lineHeight: 42,
+      color: "#464646",
+      fontFamily: "montserratBold"
+  },
+
+  carrinho: {
+      paddingVertical: 8,
+      paddingHorizontal: 16
+  },
+
+  nomeFazenda: {
+      fontSize: 16,
+      lineHeight: 26,
+      marginLeft: 12,
+      fontWeight: "bold"
+  },
+
+  imagemFazenda: {
+      width: 32,
+      height: 32
+  },
+
+  fazenda: {
+      flexDirection: "row",
+      paddingVertical: 12
+  },
+
+  descricao: {
+      color: "#A3A3A3",
+      fontStyle: "italic"
+  },
+
+  preco: {
+      fontSize: 26,
+      lineHeight: 42,
+      color: "#2A9F85",
+      marginTop: 8
+  }
 })
